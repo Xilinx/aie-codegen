382318782d9bdb626cfd1a1a98dde73d6bffd7ce
koteswara rao reddy <koteswararao.reddy@amd.com>
2024-10-21 22:06:07 -0700

    [Drv-Src] Fixing incomplete debug messages from AIE-RT
    Thic CL contains fix for following:
       Post the fixing of the CERT-C issues a bug got introduced into the AIE-RT
       which was causing incomplete debug prints to be printed on the console.
    
       The condition added to check for the return value of fprintf and vprintf
       is wrong. If the return value is -1 then its error else its success.

--- driver/src/common/xaie_helper.c
+++ driver/src/common/xaie_helper.c
@@ -349,11 +349,11 @@ void XAie_Log(FILE *Fd, const char *prefix, const char *func, u32 line,
 {
 	va_list ArgPtr;
 	va_start(ArgPtr, Format);
-	if (fprintf(Fd, "%s %s():%u: ", prefix, func, line)) {
+	if (fprintf(Fd, "%s %s():%u: ", prefix, func, line) == -1) {
 		va_end(ArgPtr);
 		return;
 	}
-	if (vfprintf(Fd, Format, ArgPtr)){
+	if (vfprintf(Fd, Format, ArgPtr) == -1){
 		va_end(ArgPtr);
 		return;
 	}

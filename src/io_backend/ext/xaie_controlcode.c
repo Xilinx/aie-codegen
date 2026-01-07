af6583ea448a9307f9a5f1504cf72c733406bbba
Reddy, Koteswara Rao <KoteswaraRao.Reddy@amd.com>
2026-01-06 16:08:09 -0800

    [CR-1258855] Enhanced control code backend to support new directives (#1074)
    This CL contains following code changes:
        1) Code changes to support addition of two new directives to every
           ASM file generated by aie-rt using control code backend.
                A) .target <arch>-<sub-arch>
                      Where
                          <arch> specifies the aie architecture
                                 e.g aie2ps, aie4
                          <sub-arch> optional, if available specifies sub architecture.
                                 e.g a/b/z for aie4
                B) .aie_row_topology A-B-C-D
                      Where,
                          A is number_of_south_shim in a column,
                          B is number_of_memtile_row in a column,
                          C is number_of_coretile_row in a column,
                          D is number_of_north_shim in a column
    
    Signed-off-by: koteswara rao reddy <koteswararao.reddy@amd.com>

--- src/io_backend/ext/xaie_controlcode.c
+++ src/io_backend/ext/xaie_controlcode.c
@@ -2093,7 +2093,7 @@ AieRC XAie_OpenControlCodeFile(XAie_DevInst *DevInst, const char *FileName, u32 
 		return XAIE_ERR;
 	}
 	printf("Generating: %s\n", FileName);
-/*
+
 	if(DevInst->DevProp.DevGen == XAIE_DEV_GEN_AIE4) {
 		fprintf(ControlCodeInst->ControlCodefp, ".target\t aie4\n");
 	}
@@ -2112,7 +2112,7 @@ AieRC XAie_OpenControlCodeFile(XAie_DevInst *DevInst, const char *FileName, u32 
 	fprintf(ControlCodeInst->ControlCodefp, ".aie_row_topology\t %d-%d-%d-%d\n", 
 		DevInst->ShimTileNumRowsSouth, DevInst->MemTileNumRows, 
 		DevInst->AieTileNumRows, DevInst->ShimTileNumRowsNorth);
-*/
+
 	fprintf(ControlCodeInst->ControlCodefp, ".partition\t %dcolumn\n",DevInst->NumCols);
 	fprintf(ControlCodeInst->ControlCodefp, ";\n");
 	fprintf(ControlCodeInst->ControlCodefp, ";text\n");
